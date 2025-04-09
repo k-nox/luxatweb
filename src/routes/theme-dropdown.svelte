87cb56005a82735af75f8026178bd06c4753f6c2
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-04-09 15:22:31 -0700

    style theme dropdown

--- src/routes/theme-dropdown.svelte
+++ src/routes/theme-dropdown.svelte
@@ -12,23 +12,36 @@
 	let { theme, setTheme }: Props = $props();
 </script>
 
+{#snippet item(option: ThemeChoice)}
+	<DropdownMenu.Item
+		class="data-highlighted:bg-hlmed rounded-sm p-1 text-sm font-medium select-none"
+		onSelect={() => setTheme(option)}>{option}</DropdownMenu.Item
+	>
+{/snippet}
+
 <DropdownMenu.Root>
-	<DropdownMenu.Trigger>
+	<DropdownMenu.Trigger
+		class="text-text hover:bg-overlay inline-flex h-10 w-10 items-center justify-center rounded-md select-none active:scale-[0.98]"
+	>
 		{#if theme === 'light'}
-			<Sun />
+			<Sun class="h-6 w-6" />
 		{:else}
-			<Moon />
+			<Moon class="h-6 w-6" />
 		{/if}
 	</DropdownMenu.Trigger>
 	<DropdownMenu.Portal>
-		<DropdownMenu.Content forceMount>
+		<DropdownMenu.Content
+			class="bg-overlay border-muted rounded-md border p-1 shadow-sm"
+			sideOffset={8}
+			forceMount
+		>
 			{#snippet child({ wrapperProps, props, open })}
 				{#if open}
 					<div {...wrapperProps}>
 						<div {...props} transition:fly>
-							<DropdownMenu.Item onSelect={() => setTheme('dark')}>dark</DropdownMenu.Item>
-							<DropdownMenu.Item onSelect={() => setTheme('light')}>light</DropdownMenu.Item>
-							<DropdownMenu.Item onSelect={() => setTheme('system')}>system</DropdownMenu.Item>
+							{@render item('dark')}
+							{@render item('light')}
+							{@render item('system')}
 						</div>
 					</div>
 				{/if}
