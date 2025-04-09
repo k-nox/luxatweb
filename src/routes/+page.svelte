271321d49acdc26c845cbf4b9efdeacc722a28a8
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-04-09 15:56:22 -0700

    flip margin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="-mt-20 flex h-full flex-col items-center justify-center">
+<div class="-mb-20 flex h-full flex-col items-center justify-center">
 	<h1 class="text-8xl/snug">hi, i'm lux</h1>
 	<p class="text-lg/snug">a software engineer, occassional poet, and communication nerd</p>
 </div>
