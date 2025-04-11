6fee33406b46420d1d25be107087f4ceb3315536
Lux Barker <29106316+k-nox@users.noreply.github.com>
2025-04-11 09:02:08 -0700

    animated link should be inline

--- src/lib/components/animated-link.svelte
+++ src/lib/components/animated-link.svelte
@@ -7,8 +7,8 @@
 	let { url, text: title }: Props = $props();
 </script>
 
-<div
+<span
 	class="text-pine before:bg-foam hover:text-foam after:bg-foam relative transition-all duration-700 ease-in-out before:absolute before:bottom-0 before:left-1/2 before:h-px before:w-0 before:origin-center before:transition-[width] before:duration-700 before:ease-in-out after:absolute after:right-1/2 after:bottom-0 after:h-px after:w-0 after:origin-center after:transition-[width] after:duration-700 after:ease-in-out hover:before:w-1/2 hover:after:w-1/2"
 >
 	<a href={url}>{title}</a>
-</div>
+</span>
